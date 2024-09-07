3888e292ab2d59d22de53729fcc233ef7119ce56
Murray5577 <limeyi35@gmail.com>
2024-09-07 11:26:38 +0800

    admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
+router.get('/restaurants/create', adminController.createRestaurants) // 順序在:id之前，以免被解讀成create
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.get('/restaurants/create', adminController.createRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
