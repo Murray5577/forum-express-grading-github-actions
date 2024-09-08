f739092e03ea427f68f0742f03a8c7591a189f4c
Murray5577 <limeyi35@gmail.com>
2024-09-08 12:39:43 +0800

    admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurants) // 順序在:id之前，以免被解讀成create
+router.get('/restaurants/:id/edit', adminController.editRestaurant) // 嚴格往上放
 router.get('/restaurants/:id', adminController.getRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
