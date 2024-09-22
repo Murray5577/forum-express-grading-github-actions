b9c25d33db9acd0c7acc5772542319e386203e33
Murray5577 <limeyi35@gmail.com>
2024-09-22 12:21:25 +0800

    category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -13,6 +13,8 @@ router.get('/restaurants', adminController.getRestaurants)
 router.get('/users', adminController.getUsers)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategories)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategories)
 
 router.patch('/users/:id', adminController.patchUser)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
