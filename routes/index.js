b3eff8ed1569dca54994aa435c1b4973f3fa6a36
Murray5577 <limeyi35@gmail.com>
2024-10-20 16:58:53 +0800

    get user's favorited restaurants & switch button

--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,15 @@ router.get('/logout', userController.logout)
 
 router.use('/admin', authenticatedAdmin, admin)
 
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
