c49b0e1b15d1bd25aa58d25fd7322381314b5669
Murray5577 <limeyi35@gmail.com>
2024-10-23 11:20:05 +0800

    add TopUser page

--- routes/index.js
+++ routes/index.js
@@ -34,11 +34,11 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
-
 router.use('/', generalErrorHandler)
 module.exports = router
